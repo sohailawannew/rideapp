d3656f866823a0ab038e9f11cf0344b669587bf4
sohailawannew <sohalawan@gmail.com>
2025-06-20 04:10:31 +0400

    sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,13 @@
-// RideApp Service Worker - Minimal v3.2
-const CACHE_NAME = 'rideapp-v3.2';
-const ASSETS = [
-  '/rideapp/',
-  '/rideapp/index.html',
-  '/rideapp/manifest.json',
-  '/rideapp/icon.png',
-  '/rideapp/icon-192x192.png'
-  // Add other files here
-];
+// Ultra-simple service worker
+const CACHE_NAME = 'rideapp-emergency-fix';
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
-});
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('activate', () => self.clients.claim());
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     fetch(e.request)
-      .catch(() => caches.match(e.request))
       .catch(() => caches.match('/rideapp/index.html'))
   );
 });
